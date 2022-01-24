b01b6b1ee78bc67b5114e73694a5a26ec18259d6
Titus Owuor <54272773+titusowuor30@users.noreply.github.com>
2022-01-24 13:59:18 +0300

    Update Constants.java

--- app/src/main/java/com/tdbsoftloanapps/smartloan/Constants.java
+++ app/src/main/java/com/tdbsoftloanapps/smartloan/Constants.java
@@ -14,7 +14,4 @@ public class Constants {
     public static final String TRANSACTION_TYPE = "CustomerPayBillOnline";
     public static final String PARTYB = "174379"; //same as business shortcode above
     public static final String CALLBACKURL = "http://mpesa-requestbin.herokuapp.com/14vfhf01";
-
-    //You should get these values from the earlier post Part 1
-
 }
